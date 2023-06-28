e27dd60bdd22fc58159f3b1301f839d2375d2da0
Lucas Sileo <lucastoledo358@gmail.com>
2023-06-28 16:31:06 -0300

    INITIAL SERVER

---
+++ prisma/migrations/20230628192736_first_migration/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "quantidade" INTEGER NOT NULL
+);
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import  Express  from "express";
+import cors from "cors"
+import bodyParser from "body-parser";
+
+const app = Express()
+
+
+const corsConfig = {
+  origin: true, //Permite acesso de qualquer endereço front end
+  optionsSuccesStatus: 200, //Status quando requisição for bem suscedidda
+}
+
+
+
+app.listen(3333, () => {
+  console.log('Server Rodando em http://localhost:3333 🚀')
+})
+
+app.use(cors(corsConfig))
+app.use(bodyParser.json())
